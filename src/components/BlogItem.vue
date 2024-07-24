9ca3fb850257cc3bb57d632a59e95748bdd1eae1
Nastya <minnlinney@gmail.com>
2024-07-24 12:28:21 +0300

    dogs and cats supremacy

---
+++ src/components/BlogItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+
+        <h5>{{ user }}</h5>
+        <p>{{ post }}</p>
+
+        <div>
+            <button @click="likeItem">Like</button>
+            <p>{{ likes }}</p>
+            <button @click="dislikeItem">Dislike</button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: Number,
+            user: String, //utilizator
+            post: String, //postarea
+            likes: Number,
+            tag: String   //filtru de gen ...
+        },
+
+
+        emits: ['like', 'dislike'],
+
+        methods: {
+            likeItem() {
+                this.$emit("like", {itemId: this.id})
+            },
+
+            dislikeItem() {
+                this.$emit("dislike", {itemId: this.id})
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/BlogList.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <h1>Blogul GGIT FD</h1>
+        <div v-if="blogList.length > 0">
+            <BlogItem v-for="blog in blogList" :key="blog.id" :id="blog.id" :user="blog.user" :post="blog.post"
+                :likes="blog.likes" :tag="blog.tag" @like="likeHandler($event)" @dislike="dislikeHandler($event)" />
+        </div>
+        <div v-else>
+            <p>Nici un post nu a fost creat</p>
+        </div>
+        <div>
+            <input v-model="user" />
+            <textarea v-model="post"></textarea>
+            <select v-model="tag">
+                <option value="general">General</option>
+                <option value="flame">Flame</option>
+                <option value="recommendation">Recommendation</option>
+            </select>
+            <button @click="submitFormHandler">Save</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { myaxios } from '../axios';
+import BlogItem from '../components/BlogItem.vue';
+export default {
+    components: { BlogItem },
+    data() {
+        return {
+            user: "",
+            post: "",
+            tag: "",
+            blogList: []
+        }
+    },
+    methods: {
+        submitFormHandler(event) {
+            const newBlog = {
+                user: this.user,
+                post: this.post,
+                tag: this.tag
+            };
+
+            //Trimitem la backend obiectul noi
+            myaxios.post("/blog", newBlog).then((response) => {
+                this.blogList = response.data.blogs
+            })
+
+            console.log(newBlog)
+        },
+        fetchBlogPosts() {
+            myaxios.get("/blog")
+                .then((response) => {
+                    console.log(response.data.blogs)
+                    this.blogList = response.data.blogs
+                    // Assuming the response data is an array of blog posts
+
+                })
+                .catch((error) => {
+                    console.error("Failed to fetch blog posts:", error);
+                });
+        },
+        likeHandler(event) {
+        console.log(event.itemId)
+        myaxios.post(`/blog/${event.itemId}/like`).then((response) => {
+            console.log(response.data.blogs)
+            this.blogList = response.data.blogs
+        })
+        },
+        dislikeHandler(event) {
+            console.log(event.itemId)
+            myaxios.post(`/blog/${event.itemId}/dislike`).then((response) => {
+            console.log(response.data.blogs)
+            this.blogList = response.data.blogs
+        })
+        }
+    },
+    mounted() {
+        this.fetchBlogPosts();
+    },
+}
+
+
+</script>
+
+<style lang="scss" scoped></style>
